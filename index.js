4d0b8ede481506c51435378ae0a80776c21a856f
jyuuuubear <jyuuuubear@gmail.com>
2023-04-19 17:49:46 +0900

    template literal

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// 식별자가 먼저 실행된다고 했는데 그럼 할당과 동시에 값을 생성해야하는 변수는?
+//templete literal
 
-console.log(sum); //undefined
-var sum = 10 + 20;
-console.log(sum); //30
+var first = `Ung-mo`;
+var last = `Lee`;
 
-// 실행결과 값을 생성하는게 먼저 
-var add = 1;
-
-console.log(sum);
+console.log(`my name is ${first} ${last}.`);
